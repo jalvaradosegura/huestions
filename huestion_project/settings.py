8a445c5df26cd449e408bdc5ca22298458725ab3
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2020-10-16 23:41:45 -0300

    Create a Questions app and add it to the settings

--- huestion_project/settings.py
+++ huestion_project/settings.py
@@ -29,8 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+THIRD_PARTY_APPS = []
+LOCAL_APPS = [
+        'questions.apps.QuestionsConfig',
+]
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
