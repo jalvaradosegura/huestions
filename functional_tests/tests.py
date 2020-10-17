04c4f6eedaac87cc7c619fd6cc24283fbb7a2a67
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2020-10-17 14:13:53 -0300

    Use Django LiveServerTestCase instead of TestCase
    Change unittest.TestCase for Django LiveServerTestCase for the
    functional test. Functional test now live within its own folder.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-import unittest
+from django.test import LiveServerTestCase
 
 from selenium import webdriver
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_visit_home_page(self):
         # Javi heard about a fun page, where you have to answer hard questions
         # She visits it
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title mention Huestion
         self.assertIn('Huestion', self.browser.title)
@@ -22,7 +22,3 @@ class NewVisitorTest(unittest.TestCase):
         # She is received with a question
         question = self.browser.find_element_by_tag_name('h1').text
         self.assertEqual('None', question)
-
-
-if __name__ == '__main__':
-    unittest.main()
