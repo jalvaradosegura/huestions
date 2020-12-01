bd849f300f9cee1b6c756579ab8ab9abe0b95f23
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2020-12-01 01:26:32 -0300

    Add test case for the get_random_question_for_user
    Add test case for when the function returns None.

--- questions/tests/test_utils.py
+++ questions/tests/test_utils.py
@@ -23,6 +23,13 @@ class UtilsTests(TestCase):
             question.question, ['Who is stronger?', 'Who is better?']
         )
 
+    def test_get_random_question_for_user_but_he_already_answered_all(self):
+        self.make_the_test_user_answer_all_the_questions()
+        question = get_random_question_for_user(self.user)
+        self.assertEqual(
+            question, None
+        )
+
     def test_get_possible_questions_for_user_amount(self):
         questions_amount = len(get_possible_questions_for_user(self.user))
         self.assertEqual(questions_amount, 2)
@@ -32,9 +39,12 @@ class UtilsTests(TestCase):
         all_questions = list(Question.objects.all())
         self.assertEqual(possible_questions, all_questions)
 
-    def test_get_possible_questions_for_user_returns_empty(self):
+    def test_get_possible_questions_for_user_returns_none(self):
+        self.make_the_test_user_answer_all_the_questions()
+        possible_questions = get_possible_questions_for_user(self.user)
+        self.assertEqual(possible_questions, None)
+
+    def make_the_test_user_answer_all_the_questions(self):
         for question in Question.objects.all():
             first_alternative = question.alternatives.all()[0]
             first_alternative.users.add(self.user)
-        possible_questions = get_possible_questions_for_user(self.user)
-        self.assertEqual(possible_questions, [])
--- questions/utils.py
+++ questions/utils.py
@@ -5,7 +5,10 @@ from .models import Question
 
 def get_random_question_for_user(user):
     possible_questions = get_possible_questions_for_user(user)
-    random_number = random.randrange(len(possible_questions))
+    try:
+        random_number = random.randrange(len(possible_questions))
+    except TypeError:
+        return None
     return possible_questions[random_number]
 
 
@@ -16,4 +19,6 @@ def get_possible_questions_for_user(user):
         for question in all_questions
         if question.has_the_user_already_voted(user) is False
     ]
+    if possible_questions == []:
+        return None
     return possible_questions
