ccc811e076bbe8e23170b81073c859dc45b4002b
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-01-09 11:20:28 -0300

    Add the ability to run tests in a parallel way

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+import multiprocessing
 import os
 import sys
 
 
 def main():
+    # Allow to activate parallelization with python 3.8 and Django 3.1.X
+    multiprocessing.set_start_method("fork")
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huestion_project.settings')
     try:
