9aaa1d231f3b75b405b016de4be32c05a4c962e3
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-01-20 09:27:21 -0300

    Expand admin page for models

--- questions/admin.py
+++ questions/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from .models import Alternative, Question, QuestionList
 
-admin.site.register(Alternative)
-admin.site.register(Question)
-admin.site.register(QuestionList)
+
+@admin.register(Alternative)
+class AlternativeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'question')
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'child_of')
+
+
+@admin.register(QuestionList)
+class QuestionListAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
