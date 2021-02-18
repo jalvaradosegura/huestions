28211297a453257096b79e1679168d0134babe81
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-02-18 00:57:46 -0300

    Add test for UserListView
    Check that the lists displayed belongs to the user.

--- users/tests.py
+++ users/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 from .views import UserListsView
-from questions.factories import UserFactory
+from questions.factories import QuestionListFactory, UserFactory
 
 
 class UserListsViewTests(TestCase):
@@ -18,3 +18,20 @@ class UserListsViewTests(TestCase):
         response = self.client.get('/users/javi/lists/')
 
         self.assertTemplateUsed(response, 'user_lists.html')
+
+    def test_contains_only_user_lists(self):
+        user_1 = UserFactory(username='javi')
+        user_2 = UserFactory(username='jorge')
+        QuestionListFactory(title='list 1', owner=user_1)
+        QuestionListFactory(title='list 2', owner=user_1)
+        QuestionListFactory(title='list 3', owner=user_2)
+
+        response_1 = self.client.get('/users/javi/lists/')
+        html_1 = response_1.content.decode('utf-8')
+        response_2 = self.client.get('/users/jorge/lists/')
+        html_2 = response_2.content.decode('utf-8')
+
+        self.assertRegex(html_1, 'list 1')
+        self.assertRegex(html_1, 'list 2')
+        self.assertNotRegex(html_1, 'list 3')
+        self.assertRegex(html_2, 'list 3')
