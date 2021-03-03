2eba8d81f4c0e88315d212d06c7b898714b35c7e
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-03-03 09:50:18 -0300

    Move forms tests of List to lists app

--- questions/tests/test_forms.py
+++ questions/tests/test_forms.py
@@ -3,7 +3,6 @@ from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from ..constants import LIST_COMPLETION_ERROR_MESSAGE
 from ..factories import (
     AlternativeFactory,
     QuestionFactory,
@@ -15,8 +14,6 @@ from ..forms import (
     CreateQuestionForm,
 )
 from ..models import Alternative, Question
-from lists.forms import CompleteListForm, CreateQuestionListForm, EditListForm
-from lists.models import QuestionList
 
 
 class AnswerQuestionFormTests(TestCase):
@@ -98,79 +95,6 @@ class AnswerQuestionFormViewTests(TestCase):
             email='javi@email.com', username='javi', password='password123'
         )
         self.client.login(email='javi@email.com', password='password123')
-
-
-class CreateQuestionListFormTests(TestCase):
-    def test_get_form_success(self):
-        self.sign_up()
-
-        response = self.client.get('/lists/create/')
-
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(response, '<label for="id_title">Title:')
-
-    def test_create_question_list_with_form(self):
-        self.sign_up()
-        form = CreateQuestionListForm(
-            data={'title': 'Super List'}, owner=self.user
-        )
-        form.save()
-
-        question_list = QuestionList.objects.last()
-
-        self.assertEqual(question_list.__str__(), 'Super List')
-
-    def test_create_two_question_lists(self):
-        self.sign_up()
-        form = CreateQuestionListForm(
-            data={'title': 'Super List 1'}, owner=self.user
-        )
-        form.save()
-        form = CreateQuestionListForm(
-            data={'title': 'Super List 2'}, owner=self.user
-        )
-        form.save()
-
-        self.assertEqual(QuestionList.objects.count(), 2)
-
-    def test_user_gets_added_to_list_after_creating_one(self):
-        self.sign_up()
-        form = CreateQuestionListForm(
-            data={'title': 'Super List 1'}, owner=self.user
-        )
-        form.save()
-
-        question_list = QuestionList.objects.last()
-
-        self.assertEqual(question_list.owner.username, 'javi')
-
-    def sign_up(self):
-        self.user = get_user_model().objects.create_user(
-            email='javi@email.com', username='javi', password='password123'
-        )
-        self.client.login(email='javi@email.com', password='password123')
-
-
-class CompleteListFormTests(TestCase):
-    def test_complete_list_with_form_fail(self):
-        question_list = QuestionListFactory(title='an awesome list')
-
-        form = CompleteListForm(question_list=question_list)
-        form.is_valid()
-
-        self.assertEqual(
-            form.custom_error_message, LIST_COMPLETION_ERROR_MESSAGE
-        )
-
-    def test_complete_list_with_form_success(self):
-        question_list = QuestionListFactory(title='an awesome list')
-        question = QuestionFactory(title='cool?', child_of=question_list)
-        AlternativeFactory(title='yes', question=question)
-        AlternativeFactory(title='no', question=question)
-
-        form = CompleteListForm(question_list=question_list)
-
-        self.assertEqual(form.custom_error_message, '')
 
 
 class CreateQuestionFormTests(TestCase):
@@ -279,14 +203,3 @@ class AddAlternativesFormTests(TestCase):
             email='javi@email.com', username='javi', password='password123'
         )
         self.client.login(email='javi@email.com', password='password123')
-
-
-class EditListFormTests(TestCase):
-    def test_create_list_with_form(self):
-        form = EditListForm(data={'title': 'Is this working?'})
-        form.save()
-        question_list = QuestionList.objects.last()
-
-        self.assertEqual(question_list.__str__(), 'Is this working?')
-        self.assertEqual(question_list.slug, 'is-this-working')
-        self.assertEqual(QuestionList.objects.all().count(), 1)
