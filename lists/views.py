ae81bec406331c98caccee30855a52989f8da1ad
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-03-04 00:40:48 -0300

    Remove unused import

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render, reverse
 from django.views.generic import DeleteView, DetailView, ListView, UpdateView
 
