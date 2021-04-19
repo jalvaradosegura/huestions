383d5ea7770e0fdadee1d391005cc4faa7be5852
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-04-19 11:35:41 -0400

    Add translation to the name "Home"

--- huestion_project/settings/local_gcp.py
+++ huestion_project/settings/local_gcp.py
@@ -10,16 +10,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 INSTALLED_APPS += [
     'django_extensions',
-    'debug_toolbar',
 ]
 
-# django-debug-toolbar
-MIDDLEWARE += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
 
 DATABASES = {
     'default': {
