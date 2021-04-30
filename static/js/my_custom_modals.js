a3fafdb7cb7aac52cee68a4771b0595a223e8010
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-04-29 23:07:24 -0400

    Add image info in answer question html

--- static/js/my_custom_modals.js
+++ static/js/my_custom_modals.js
@@ -1,17 +1,36 @@
-var button = document.getElementById('my-modal-button-1');
-var modal = document.getElementById('my-modal-1');
-var close = document.getElementById('my-modal-close-1');
+var button1 = document.getElementById('my-modal-button-1');
+var modal1 = document.getElementById('my-modal-1');
+var close1 = document.getElementById('my-modal-close-1');
+var text1 = document.getElementById('modal-text-1');
+var input_text1 = document.getElementById('my-modal-image-text-1');
 
-button.onclick = function() {
-  modal.style.display = 'block';
+var button2 = document.getElementById('my-modal-button-2');
+var modal2 = document.getElementById('my-modal-2');
+var close2 = document.getElementById('my-modal-close-2');
+var text2 = document.getElementById('modal-text-2');
+var input_text2 = document.getElementById('my-modal-image-text-1');
+
+button1.onclick = function() {
+  modal1.style.display = 'block';
+  text1.innerHTML = input_text1.innerHTML;
 }
 
-close.onclick = function() {
-  modal.style.display = 'none';
+close1.onclick = function() {
+  modal1.style.display = 'none';
+}
+
+button2.onclick = function() {
+  modal2.style.display = 'block';
+  text2.innerHTML = input_text2.innerHTML;
+}
+
+close2.onclick = function() {
+  modal2.style.display = 'none';
 }
 
 window.onclick = function(event) {
   if (event.target.className == 'modal-background') {
-    modal.style.display = 'none';
+    modal1.style.display = 'none';
+    modal2.style.display = 'none';
   }
 }
